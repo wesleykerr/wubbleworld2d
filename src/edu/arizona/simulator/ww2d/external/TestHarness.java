cbd1c067e9c50c8d8e5f087274c808f57a4d35aa
deerslyr1 <deerslyr1@98741d97-488c-615d-27b5-d31cdc3f2f5b>
2010-10-29 00:12:51 +0000

    Added a test case and realized that I forgot to add states to the list of attributes.

--- src/edu/arizona/simulator/ww2d/external/TestHarness.java
+++ src/edu/arizona/simulator/ww2d/external/TestHarness.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import edu.arizona.verbs.mdp.OOMDPObjectState;
 import edu.arizona.verbs.mdp.OOMDPState;
+import edu.arizona.verbs.mdp.Relation;
 
 public class TestHarness {
 	
@@ -72,11 +73,61 @@ public class TestHarness {
 		System.out.println(env.performAction("agent1 1000;agent2 1000"));
 		System.out.println(env.performAction("agent1 1000;agent2 1000"));
 		
-//		env.cleanup();
+		env.cleanup();
 	}
+	
+	public static void test3Agent() { 
+		OOMDPObjectState obj1 = new OOMDPObjectState("agent1", "agent");
+		obj1.setAttribute("x", "10");
+		obj1.setAttribute("y", "10");
+		obj1.setAttribute("heading", "0.00");
+		obj1.setAttribute("shape-type", "circle");
+		obj1.setAttribute("radius", "0.25");
+
+		OOMDPObjectState obj2 = new OOMDPObjectState("agent2", "agent");
+		obj2.setAttribute("x", "20");
+		obj2.setAttribute("y", "10");
+		obj2.setAttribute("heading", "3.14");
+		obj2.setAttribute("shape-type", "circle");
+		obj2.setAttribute("radius", "0.25");
+		
+		List<OOMDPObjectState> objects = new ArrayList<OOMDPObjectState>();
+		objects.add(obj1);
+		objects.add(obj2);
+		
+		WW2DEnvironment env = new WW2DEnvironment(true);
+		OOMDPState state = env.initializeEnvironment(objects);
+
+		System.out.println("Initialization complete...");
+		System.out.println(state.toString());
+		
+		System.out.println("Printing actions...");
+		List<String> actions = env.getActions();
+		System.out.println(actions.size() + " actions...");
+		System.out.println(env.getActions());
+
+		System.out.println(env.performAction("agent1 1000;agent2 1000"));
+		System.out.println(env.performAction("agent1 1000;agent2 1000"));
+		System.out.println(env.performAction("agent1 1000;agent2 1000"));
+		System.out.println(env.performAction("agent1 1000;agent2 1000"));
+		
+		obj1.setAttribute("vx", "-30.00");
+		obj1.setAttribute("vy", "0.00");
+		obj1.setAttribute("vtheta", "0.000");
+
+		obj2.setAttribute("vx", "1.00");
+		obj2.setAttribute("vy", "0.00");
+		obj2.setAttribute("vtheta", "0.000");
+
+		System.out.println(env.simulateAction(new OOMDPState(objects, new ArrayList<Relation>()), "agent1 1000;agent2 1000"));
+		
+		env.cleanup();
+	}
+	
 	
 	public static void main(String[] args) { 
 //		test1Agent();
-		test2Agent();
+//		test2Agent();
+		test3Agent();
 	}
 }
