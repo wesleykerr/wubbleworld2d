e712c666c5d4f1f6f8453092a661e571a96d6596
deerslyr1 <deerslyr1@98741d97-488c-615d-27b5-d31cdc3f2f5b>
2010-11-21 20:16:26 +0000

    Making the default to be record.

--- src/edu/arizona/simulator/ww2d/utils/GameGlobals.java
+++ src/edu/arizona/simulator/ww2d/utils/GameGlobals.java
@@ -6,7 +6,7 @@ public class GameGlobals {
 	
 	public static NumberFormat nf;
 	
-	public static boolean record = false;
+	public static boolean record = true;
 
     static { 
 		nf = NumberFormat.getInstance();
